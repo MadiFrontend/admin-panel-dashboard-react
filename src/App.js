b3de19e266bc349abe4c7c0a2a49de8514724d29
mahdi <mahdihosseinidev@gmail.com>
2022-04-26 11:44:13 +0430

    sidebar & navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Sidebar from "./components/sidebar/Sidebar";
+import TopBar from "./components/TopBar/TopBar";
+import "./app.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <TopBar />
+            <div className="container">
+                <Sidebar />
+                <div className="others">Other Pages</div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
