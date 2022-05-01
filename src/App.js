d380aa2bcdae6a4ff4736457260bc012595f8c03
mahdi <mahdihosseinidev@gmail.com>
2022-05-01 17:54:07 +0430

    add user & edit userList

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-
 //Components
 
 import Sidebar from "./components/sidebar/Sidebar";
 import TopBar from "./components/TopBar/TopBar";
 import Home from "./pages/home/Home";
+import UserList from "./pages/userList/UserList";
+import User from "./pages/user/User";
+import NewUser from "./pages/newUser/NewUser";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 //Styles
 import "./app.css";
 
 function App() {
-    
-
     return (
-        <div className="App">
+        <BrowserRouter>
             <TopBar />
             <div className="container">
                 <Sidebar />
-                <Home />
+                <Routes>
+                    <Route path="/" element={<Home />}></Route>
+                    <Route path="users" element={<UserList />}></Route>
+                    <Route path="user/:userId" element={<User/>}></Route>
+                    <Route path="/newUser" element={<NewUser/>}></Route>
+                </Routes>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
