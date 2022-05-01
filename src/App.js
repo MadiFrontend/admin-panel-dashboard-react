199c1d6cc8a230068a4f3c0bced387fdde0f96eb
mahdi <mahdihosseinidev@gmail.com>
2022-05-01 20:12:36 +0430

    Add Product part

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Home from "./pages/home/Home";
 import UserList from "./pages/userList/UserList";
 import User from "./pages/user/User";
 import NewUser from "./pages/newUser/NewUser";
+import Product from "./pages/product/Product";
+import ProductList from "./pages/productList/ProductList";
+import NewProduct from "./pages/newProduct/NewProduct";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 //Styles
@@ -22,6 +25,10 @@ function App() {
                     <Route path="users" element={<UserList />}></Route>
                     <Route path="user/:userId" element={<User/>}></Route>
                     <Route path="/newUser" element={<NewUser/>}></Route>
+
+                    <Route path="products" element={<ProductList />}></Route>
+                    <Route path="product/:productId" element={<Product/>}></Route>
+                    <Route path="/newproduct" element={<NewProduct/>}></Route>
                 </Routes>
             </div>
         </BrowserRouter>
